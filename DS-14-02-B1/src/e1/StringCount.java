35ffb950d74d0f42d5a5dbe692dec96d0628628b
migueldeoleiros <migueldeoleiros@gmail.com>
2021-09-21 11:06:23 +0200

    Los test funcionan ahora

--- DS-14-02-B1/src/e1/StringCount.java
+++ DS-14-02-B1/src/e1/StringCount.java
@@ -1,24 +1,28 @@
 package e1;
 
 public class StringCount {
-/**
-* Counts the number of words in a given String.
+
+/** * Counts the number of words in a given String.
 * Words are groups of characters separated by one or more spaces.
 *
 * @param text String with the words
-* @return Number of words in the String or zero if it is null */
+* @return Number of words in the String or zero if it is null
+*/
 public static int countWords (String text) {
 
 }
+
 /**
 * Counts the number of times the given character appears in the String.
 * Accented characters are considered different characters .
 * @param text String with the characters
 * @param c the character to be found
-* @return Number of times the character appears in the String or zero if null */
+* @return Number of times the character appears in the String or zero if null
+*/
 public static int countChar(String text , char c) {
 
 }
+
 /** * Counts the number of times the given character appears in the String.
 * The case is ignored so an ’a’ is equal to an ’A ’.
 * Accented characters are considered different characters .
@@ -29,6 +33,7 @@ public static int countChar(String text , char c) {
 public static int countCharIgnoringCase (String text , char c) {
 
 }
+
 /**
 * Checks if a password is safe according to the following rules:
 * - Has at least 8 characters
@@ -36,10 +41,11 @@ public static int countCharIgnoringCase (String text , char c) {
 * - Has a lowercase character
 * - Has a digit
 * - Has a special character among these: ’?’, ’@’, ’#’, ’$’, ’.’ and ’,’
-* @param password The password , we assume it is not null.
-* @return true if the password is safe , false otherwise
-*/
+ * @param password The password , we assume it is not null.
+ * @return true if the password is safe , false otherwise
+ */
 public static boolean isPasswordSafe (String password) {
 
 }
+
 }
