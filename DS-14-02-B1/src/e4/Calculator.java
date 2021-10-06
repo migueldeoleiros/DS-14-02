345a22015279dbcd2f2cd02cd2c37557c97055f5
migueldeoleiros <migueldeoleiros@gmail.com>
2021-10-06 12:26:55 +0200

    ejercicio 4 completo

--- DS-14-02-B1/src/e4/Calculator.java
+++ DS-14-02-B1/src/e4/Calculator.java
@@ -1,8 +1,45 @@
 package e4;
 
+import java.util.ArrayList;
+
 public class Calculator {
 
-    enum operator {SUM,REST,MULT,DIV}
+    enum Operator{SUM,RES,MUL,DIV;
+        @Override
+        public String toString() {
+            return switch (this) {
+                case SUM -> "+";
+                case RES -> "-";
+                case MUL -> "*";
+                case DIV -> "/";
+            };
+        }
+        public float Operation(float valor1, float valor2) {
+            return switch (this) {
+                case SUM -> valor1 + valor2;
+                case RES -> valor1 - valor2;
+                case MUL -> valor1 * valor2;
+                case DIV -> valor1 / valor2;
+            };
+        }
+    }
+
+    ArrayList<Operator> operations = new ArrayList<>();
+    ArrayList<Float> numbers = new ArrayList<>();
+
+    private Operator toOperator(String str){
+        switch (str){
+            case "+":
+                return Operator.SUM;
+            case "-":
+                return Operator.RES;
+            case "*":
+                return Operator.MUL;
+            case "/":
+                return Operator.DIV;
+        }
+        throw new IllegalArgumentException();
+    }
 
     /**
      * Public constructor of the calculator .
@@ -15,7 +52,8 @@ public class Calculator {
      * Clean the internal state of the calculator
      */
     public void cleanOperations() {
-
+        this.operations = new ArrayList<>();
+        this.numbers = new ArrayList<>();
     }
 
     /**
@@ -33,7 +71,13 @@ public class Calculator {
      * @throws IllegalArgumentException If the operation does not exist.
      */
     public void addOperation(String operation, float... values) {
-
+        operations.add(toOperator(operation));
+        numbers.add(values[0]);
+        if(values.length == 2 ){
+            numbers.add(values[1]);
+        }else if(values.length != 1){
+            throw new IllegalArgumentException("too many arguments");
+        }
     }
 
    /**
@@ -47,8 +91,21 @@ public class Calculator {
      *                             (division by zero)
      */
     public float executeOperations() {
-        float result=0;
+        float value1=0, value2, result=0;
+        int count=1;
+        for(Operator opt: operations){
+            if(count == 1) value1=numbers.get(0);
+            value2=numbers.get(count);
 
+            if(opt == Operator.DIV && value2 == 0){
+                cleanOperations();
+                throw new ArithmeticException();
+            }
+            result = opt.Operation(value1, value2);
+
+            value1 = result; count++;
+        }
+        cleanOperations();
         return result;
     }
 
@@ -61,9 +118,16 @@ public class Calculator {
      */
     @Override
     public String toString() {
-        String screen="";
+        String screen="[STATE:";
+        if(operations.size() != 0 && numbers.size() != 0){
+            for(int i=0; i<operations.size(); i++){
+                screen += ("[" + operations.get(i) + "]");
+                if(i == 0) screen += (numbers.get(0) + "_");
+                screen += numbers.get(i+1);
+            }
+        }
 
-        return screen;
+        return screen + "]";
     }
 
 }
