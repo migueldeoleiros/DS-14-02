c0f85eb95a463d5a624d90589785600411bf42f3
migueldeoleiros <migueldeoleiros@gmail.com>
2021-10-26 09:54:10 +0200

    excepciones para numeros negativos

--- DS-14-02-B2/src/e1/Colegio.java
+++ DS-14-02-B2/src/e1/Colegio.java
@@ -11,34 +11,37 @@ public class Colegio {
 
     public Colegio(){}
 
+    private void exceptionTest(int edad, int horrocrux){
+        if(edad<=0 || horrocrux <=0)throw new IllegalArgumentException();
+    }
     public void addEstudiante(String nombre, String apellido, int edad, int horrocrux, Residente.Casa casa){
         e1.Residentes.Estudiante residente = new e1.Residentes.Estudiante();
+        exceptionTest(edad,horrocrux);
         residente.set(nombre, apellido, edad, horrocrux, casa);
-//            throw new IllegalArgumentException();
         residentes.add(residente);
     }
     public void addFantasma(String nombre, String apellido, int edad, int horrocrux, Residente.Casa casa){
         e1.Residentes.Fantasma residente = new e1.Residentes.Fantasma();
+        exceptionTest(edad,horrocrux);
         residente.set(nombre, apellido, edad, horrocrux, casa);
-//            throw new IllegalArgumentException();
         residentes.add(residente);
     }
     public void addConserje(String nombre, String apellido, int edad, int horrocrux, boolean nocturnidad){
         e1.Personales.Conserje personal = new e1.Personales.Conserje();
+        exceptionTest(edad,horrocrux);
         personal.set(nombre, apellido, edad, horrocrux, nocturnidad);
-//            throw new IllegalArgumentException();
         personales.add(personal);
     }
     public void addDocente(String nombre, String apellido, int edad, int horrocrux, Docente.Asignatura asignatura){
         e1.Personales.Docente personal = new e1.Personales.Docente();
+        exceptionTest(edad,horrocrux);
         personal.set(nombre, apellido, edad, horrocrux, asignatura);
-//            throw new IllegalArgumentException();
         personales.add(personal);
     }
     public void addGuardabosque(String nombre, String apellido, int edad, int horrocrux, boolean nocturnidad){
         e1.Personales.Guardabosque personal = new e1.Personales.Guardabosque();
+        exceptionTest(edad,horrocrux);
         personal.set(nombre, apellido, edad, horrocrux, nocturnidad);
-//            throw new IllegalArgumentException();
         personales.add(personal);
     }
 
--- DS-14-02-B2/test/e1/ColegioTest.java
+++ DS-14-02-B2/test/e1/ColegioTest.java
@@ -20,13 +20,13 @@ class ColegioTest {
     @Test
     void testRecompensa(){
         assertEquals("""
-                        Hermione Granger(Estudiante de Gryffindor, 3 horrocruxes): 270.0 galeones
-                        Baron Sanguinario(Fantasma de Slytherin, 1 horrocruxes): 160.0 galeones
-                        Rubeus Hagrid(Guardabosques, 2 horrocruxes): 150.0 galeones
-                        Minerva McGonagall(Docente de Transformaciones, 1 horrocruxes): 50.0 galeones
-                        Severus Snape(Docente de Defensa, 2 horrocruxes): 75.0 galeones
-                        Argus Filch(Conserje, 0 horrocruxes): 0.0 galeones
-                        La recompensa total del Colegio Howarts es de 705.0 galeones""",
+                Hermione Granger(Estudiante de Gryffindor, 3 horrocruxes): 270.0 galeones
+                Baron Sanguinario(Fantasma de Slytherin, 1 horrocruxes): 160.0 galeones
+                Rubeus Hagrid(Guardabosques, 2 horrocruxes): 150.0 galeones
+                Minerva McGonagall(Docente de Transformaciones, 1 horrocruxes): 50.0 galeones
+                Severus Snape(Docente de Defensa, 2 horrocruxes): 75.0 galeones
+                Argus Filch(Conserje, 0 horrocruxes): 0.0 galeones
+                La recompensa total del Colegio Howarts es de 705.0 galeones""",
                 Howarts.imprimirRecompensas());
     }
     @Test
