9f0717e04d0be6272b62eee9e7fcd108da11e387
migueldeoleiros <migueldeoleiros@gmail.com>
2021-12-14 12:13:53 +0100

    implementado ordenacion jerarquica

--- DS-14-02-PD/src/e2/OrdenJerarq.java
+++ DS-14-02-PD/src/e2/OrdenJerarq.java
@@ -1,21 +1,40 @@
 package e2;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
 public class OrdenJerarq implements TipoOrdenacion{
     @Override
     public List<Character> ordenar(Map<Character, List<Character>> nodos) {
+        List<Character> ordenada = new ArrayList<>();
+        List<Character> sinPadre = new ArrayList<>();
 
-    for (Character i : nodos.keySet()) {
-
-    }
-    for (List<Character> i : nodos.values()) {
-        for(Character j : i){
-
+        Iterator<Character> it = nodos.keySet().iterator();
+        while(it.hasNext()){
+            for(Character padre : nodos.keySet()) {
+                boolean out = false;
+                for (List<Character> listHijo : nodos.values()) {
+                    for (Character hijo : listHijo) {
+                        if (hijo == padre) {
+                            out = true;
+                            break;
+                        }
+                    }
+                    if (out) break;
+                }
+                if (!out || sinPadre.contains(padre)) {
+                    ordenada.add(padre);
+                }
+            }
+            for(Character i: ordenada){
+                if(nodos.get(i) != null)
+                    sinPadre.addAll(nodos.get(i));
+                nodos.remove(i);
+            }
+            it = nodos.keySet().iterator();
         }
-
-    }
-        return null;
+        return ordenada;
     }
 }
