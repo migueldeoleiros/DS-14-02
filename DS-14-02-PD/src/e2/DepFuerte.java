45c2f12aada7990e3fe7b18671358d6d09ba8370
migueldeoleiros <migueldeoleiros@gmail.com>
2021-12-14 10:48:19 +0100

    intento de implemtnear dependencia fuerte

--- DS-14-02-PD/src/e2/DepFuerte.java
+++ DS-14-02-PD/src/e2/DepFuerte.java
@@ -1,2 +1,29 @@
-package e2;public class DepFuerte {
+package e2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class DepFuerte implements TipoOrdenacion{
+    @Override
+    public List<Character> ordenar(Map<Character, List<Character>> nodos) {
+        List<Character> ordenada = new ArrayList<>();
+
+        for (Character padre : nodos.keySet()) {
+            boolean out=false;
+            for(List<Character> listHijo : nodos.values()) {
+                for (Character hijo : listHijo) {
+                    if(hijo == padre) {
+                        out=true;break;
+                    }
+                }
+                if(out)break;
+            }
+            if(!out){
+                ordenada.add(padre);
+                nodos.remove(padre);
+            }
+        }
+        return ordenada;
+    }
 }
